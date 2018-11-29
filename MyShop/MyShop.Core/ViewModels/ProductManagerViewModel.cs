041f522ecbe203896d6f0ef795982184b8ea157e
joelluong <luongdaitrungduong@gmail.com>
2018-11-29 19:32:05 +1100

    Product Grouping Adding

---
+++ MyShop/MyShop.Core/ViewModels/ProductManagerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.ViewModels
+{
+    using MyShop.Core.Models;
+
+    public class ProductManagerViewModel
+    {
+        public Product Product { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+
+
+    }
+}
--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -4,17 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory;
 
 namespace MyShop.WebUI.Controllers
 {
+
     public class ProductManagerController : Controller
     {
         ProductRepository context;
 
+        ProductCategoryRepository productCategories;
+
         public ProductManagerController()
         {
             this.context = new ProductRepository();
+            productCategories = new ProductCategoryRepository();
         }
 
 
@@ -28,9 +33,11 @@ namespace MyShop.WebUI.Controllers
 
         public ActionResult Create()
         {
-            Product product = new Product();
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
+            viewModel.Product = new Product();
+            viewModel.ProductCategories = this.productCategories.Collection();
 
-            return View(product);
+            return View(viewModel);
         }
 
         [HttpPost]
@@ -59,7 +66,10 @@ namespace MyShop.WebUI.Controllers
             }
             else
             {
-                return View(product);
+                ProductManagerViewModel viewModel = new ProductManagerViewModel();
+                viewModel.Product = product;
+                viewModel.ProductCategories = this.productCategories.Collection();
+                return View(viewModel);
             }
         }
 
