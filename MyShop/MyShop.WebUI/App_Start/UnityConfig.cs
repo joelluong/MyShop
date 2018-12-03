6b486f9029bdac112ecd40fe04607e01f2ae9b7a
joelluong <luongdaitrungduong@gmail.com>
2018-12-04 00:54:28 +1100

    Created Basket UI

--- MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
+++ MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
@@ -8,6 +8,7 @@ namespace MyShop.WebUI
     using MyShop.Core.Models;
     using MyShop.DataAccess.InMemory;
     using MyShop.DataAccess.SQL;
+    using MyShop.Services;
 
     /// <summary>
     /// Specifies the Unity configuration for the main container.
@@ -49,6 +50,9 @@ namespace MyShop.WebUI
             // container.RegisterType<IProductRepository, ProductRepository>();
             container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
             container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Basket>, SQLRepository<Basket>>();
+            container.RegisterType<IRepository<BasketItem>, SQLRepository<BasketItem>>();
+            container.RegisterType<IBasketService, BasketService>();
         }
     }
 }
